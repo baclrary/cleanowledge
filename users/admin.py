715201f92866bae2bb80d0d8151f4158bf6c4e18
roma <namnetsy@gmail.com>
2023-04-15 13:13:07 +0300

    add search for users in django admin

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ("username", "first_name", "middle_name", "last_name")
+
+    class Meta:
+        model = User
+
+
+admin.site.register(User, UserAdmin)
